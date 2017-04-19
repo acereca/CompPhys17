b5c6e6c9ecd0937c7ffe9fd6b600bb237689a92f
acereca <tigerauge96@live.de>
2017-04-19 15:24:52 +0200

    started ex01-2

---
+++ 01/01-2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplot.pyplot as plt
+
+a = 5
+n = np.array([1,5,10,20,30,50])
+xdata = np.linspace(0,1, 1000)
+
+y = lambda x, a, n: 1/n - a*
+
+for it in n:
+    plt.plot()
