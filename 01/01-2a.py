014de61588c5e9b40e6c4b51154a54c2c5fb39f9
acereca <tigerauge96@live.de>
2017-04-21 17:52:58 +0200

    add 01-2a and b

---
+++ 01/01-2a.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = 5
+n = np.array([1,5,10,20,30,50])
+xdata = np.linspace(0,1, 1000)
+
+y = lambda x, a, n: x**n/(x+a)
+
+
+plt.figure(figsize=(10,5))
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+for it in n:
+    plt.plot(xdata, y(xdata, a, it))
+
+plt.savefig('01-2a.png')
---
+++ 01/01-2b.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys
+import pandas as pd
+
+def y(a, n, y0):
+    if n == 0:
+        return y0
+    else:
+        return 1/n - a* y(a, n-1, y0)
+
+if __name__ == '__main__':
+    
+    # accepting args as: a, n0, y0, n1
+    
+    print(sys.argv)
+
+    a = int(sys.argv[1])
+    n0 = int(sys.argv[2])
+    n1 = int(sys.argv[4])
+    y0 = int(sys.argv[3])
+
+    ndata = range(min(n0,n1),max(n0,n1)+1)
+    print(ndata)
+    ydata = [y(a, i, y0) for i in ndata] 
+    print(ydata)
+    
+    f = open("01-2b.tex", 'w')
+    f.write(pd.DataFrame(np.array([ndata, ydata]).T, columns=['n',"y_n"]).to_latex())
+    f.close()
