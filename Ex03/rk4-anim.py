7f5deff639e5df4058669d8afbd4219a9c29e8a8
acereca <tigerauge96@live.de>
2017-05-13 12:40:28 +0200

    add userlib and MplAnimator class

--- Ex03/rk4-anim.py
+++ Ex03/rk4-anim.py
@@ -1,43 +1,7 @@
 from rk4_threebody import three_body_sim
-import matplotlib.animation as anim
-import matplotlib.patches as pat
 import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing
-import subprocess
-
-xinit = np.array([[
-    -.97000436,
-    .24308753,
-    -.46620368,
-    -.43236573,
-    .97000436,
-    -.24308753,
-    -.46620368,
-    -.43236573,
-    .0,
-    .0,
-    .93240737,
-    .86473146
-]])
-nsteps = 1000
-tmax = 4
-xlist = three_body_sim(nsteps, tmax, xinit)
-
-
-# from here mostly animation stuff
-fig = plt.figure(figsize=(19.2,10.8))
-ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
-
-#create artists before initial setup!
-line1, = ax.plot([], [], color='r', marker='.', markersize=0, linewidth=1, linestyle='-')
-point1, = ax.plot([], [], color='r', marker='.', markersize=6)
-line2, = ax.plot([], [], color='g', marker='.', markersize=0, linewidth=1, linestyle='-')
-point2, = ax.plot([], [], color='g', marker='.', markersize=6)
-line3, = ax.plot([], [], color='b', marker='.', markersize=0, linewidth=1, linestyle='-')
-point3, = ax.plot([], [], color='b', marker='.', markersize=6)
-
-#vec1 = pat.Arrow(0,0,0,0)
+from userlib.mplmpanimator import MplAnimator
 
 def init():
     line1.set_data([],[])
@@ -46,12 +10,12 @@ def init():
     point1.set_data([],[])
     point2.set_data([],[])
     point3.set_data([],[])
-    return line1,line2,line3,point1, point2,point3
+
 
 def animate(i):
     s = 1
     tail = int(nsteps/10)
-    #print("frame: {}".format(i))
+
     line1.set_data(xlist[(i*s)-tail:i*s,0],xlist[(i*s)-tail:i*s,1])
     line2.set_data(xlist[(i*s)-tail:i*s,4],xlist[(i*s)-tail:i*s,5])
     line3.set_data(xlist[(i*s)-tail:i*s,8],xlist[(i*s)-tail:i*s,9])
@@ -67,50 +31,37 @@ def animate(i):
         fontsize=14,
         bbox=dict(ec='#ffffff',fc="1")
     )
-    return line1,line2,line3, point1,point2,point3
 
-def create_file(startFrame: int, stopFrame: int, filePart: int, fps: int = 60):
-    moviewriter = anim.FFMpegWriter(fps=fps,codec='libx264')
-    fname = 'p_{:02}.mp4'.format(filePart)
-    moviewriter.setup(fig, fname, dpi=100)
-    for j in range(startFrame, stopFrame):
-        animate(j)
-        moviewriter.grab_frame()
-    moviewriter.finish()
+if __name__ == "__main__":
+    xinit = np.array([[
+        -.97000436,
+        .24308753,
+        -.46620368,
+        -.43236573,
+        .97000436,
+        -.24308753,
+        -.46620368,
+        -.43236573,
+        .0,
+        .0,
+        .93240737,
+        .86473146
+    ]])
+    nsteps = 1000
+    tmax = 4
+    xlist = three_body_sim(nsteps, tmax, xinit)
 
-def multiP(nFrames: int, nProc: int = 4):
-    p = []
-    for i in range(nProc):
-        p.append(multiprocessing.Process(target=create_file, args=(int(nFrames/nProc)*i, int(nFrames/nProc)*(i+1), i)))
-        p[i].start()
+    # from here mostly animation stuff
+    fig = plt.figure(figsize=(19.2, 10.8))
+    ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
 
-    for pr in p:
-        pr.join()
+    # create artists before initial setup!
+    line1, = ax.plot([], [], color='r', marker='.', markersize=0, linewidth=1, linestyle='-')
+    point1, = ax.plot([], [], color='r', marker='.', markersize=6)
+    line2, = ax.plot([], [], color='g', marker='.', markersize=0, linewidth=1, linestyle='-')
+    point2, = ax.plot([], [], color='g', marker='.', markersize=6)
+    line3, = ax.plot([], [], color='b', marker='.', markersize=0, linewidth=1, linestyle='-')
+    point3, = ax.plot([], [], color='b', marker='.', markersize=6)
 
-
-
-if __name__=="__main__":
-    nParts = 4
-    multiP(nsteps, nParts)
-    fname = "rk4-anim"
-    import os
-    fdir = os.path.dirname(os.path.realpath(__file__))
-
-    foundStr = ""
-    for i in range(nParts):
-        foundStr += "file " + fdir + "/p_{:02}.mp4".format(i) + "\n"
-
-    with open("temp", 'w') as f:
-        f.write(foundStr)
-
-    concatCmd = ["ffmpeg", "-f", "concat", "-safe", "0", "-i", "temp", "-c", "copy", "{}.mp4".format(fname)]
-
-    p = subprocess.Popen(concatCmd)
-    p.wait()
-
-
-    cleanupCmd = ["find", fdir, "-name", "p_[0-9][0-9].mp4", "-delete"]
-    subprocess.call(cleanupCmd)
-
-    rmTemp = ['rm', fdir +'/temp']
-    subprocess.call(rmTemp)
+    animator = MplAnimator(fig, animate, nsteps+1, "rk4-anim", initialize = init)
+    animator.start()
---
+++ userlib/mplmpanimator.py
@@ -0,0 +1,91 @@
+import multiprocessing
+import os
+import subprocess
+
+from typing import Callable
+import matplotlib as mpl
+import matplotlib.animation as anim
+
+
+class MplAnimator(object):
+
+    def __init__(self,
+                 fig: mpl.pyplot,
+                 animate: Callable[[int], None],
+                 nframes: int,
+                 filename: str,
+                 fps: int = 60,
+                 nproc: int = 4,
+                 initialize: Callable[[],None] = lambda: ""):
+
+        self.figure         = fig
+        self.num_frames     = nframes
+        self.num_fps        = fps
+        self.name_outfile   = filename
+        self.num_processes  = nproc
+        self.func_animation = animate
+        self.func_init      = initialize
+        self.cwd            = os.path.dirname(os.path.realpath(__file__))
+
+    def __create_file(self,
+                      frame_start: int,
+                      frame_stop: int,
+                      file_part: int):
+
+        moviewriter = anim.FFMpegWriter(fps=self.num_fps, codec='libx264')
+        fname = self.cwd + "/p_{:02}.mp4".format(file_part)
+        moviewriter.setup(self.figure, fname, dpi=100)
+
+        for j in range(frame_start, frame_stop):
+            self.func_animation(j)
+            moviewriter.grab_frame()
+
+        moviewriter.finish()
+
+    def __multi_processing(self):
+        list_processes = []
+        num_partframes = int(self.num_frames / self.num_processes)
+        for num_process in range(self.num_processes):
+            list_processes.append(
+                multiprocessing.Process(
+                    target=self.__create_file,
+                    args=(
+                        num_partframes * num_process,
+                        num_partframes * (num_process + 1),
+                        num_process
+                    )
+                )
+            )
+
+            list_processes[num_process].start()
+            print("started proc #{:02}, with frames {} to {}".format(
+                num_process,
+                num_partframes*num_process,
+                num_partframes*(num_process+1)-1
+            ))
+
+        for process in list_processes:
+            process.join()
+
+    def __cleanup_files(self):
+
+        str_parts = ""
+        for i in range(self.num_processes):
+            str_parts += "file " + self.cwd + "/p_{:02}.mp4".format(i) + "\n"
+
+        with open(self.cwd +"/temp", 'w') as f:
+            f.write(str_parts)
+
+        cmd_concat = ["ffmpeg", "-f", "concat", "-safe", "0", "-i", "temp", "-y", "-c", "copy", "{}.mp4".format(self.name_outfile)]
+        cmd_cleanup_parts = ["find", self.cwd, "-name", "p_[0-9][0-9].mp4", "-delete"]
+        cmd_cleanup_temp = ['rm', self.cwd + '/temp']
+
+        pipe = subprocess.Popen(cmd_concat)
+        pipe.wait()
+
+        subprocess.call(cmd_cleanup_parts)
+        subprocess.call(cmd_cleanup_temp)
+
+    def start(self):
+        self.__multi_processing()
+        self.__cleanup_files()
